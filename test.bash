cb74e2d2b0f77cf449694fa030e22ce8c0f9f72d
Aki Moto <denyuking.volval8@gmail.jp>
2023-10-26 13:03:27 +0900

    Change code

--- test.bash
+++ test.bash
@@ -10,5 +10,13 @@ res=0
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng ${LINENO}
 
+out=$(echo あ | ./plus)
+[ "$?" = 1 ] || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo | ./plus)
+[ "$?" = 1 ] || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
 [ "$res" = 0 ] && echo OK
 exit $res
