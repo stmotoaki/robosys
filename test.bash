978fd2280bfa7ffa4341f7a73c48ca64e0b4ce49
Aki Moto <denyuking.volval8@gmail.jp>
2023-11-29 16:44:40 +0900

    change

--- test.bash
+++ test.bash
@@ -11,14 +11,12 @@ out=$(./plus 1 2 3 4 5)
 [ "${out}" = 15 ] || ng ${LINENO}
 
 out=$(./plus 1 2 3 a 4 5)
-#[ "$?" = 1 ] || ng ${LINENO}
-[ "${out}" = ERROR:数字以外が足されています
-　"${out}" = 15 ] || ng ${LINENO}
+[ "$?" = 1 ] || ng ${LINENO}
+[ "${out}" = ERROR:数字以外が足されています 15 ] || ng ${LINENO}
 
 out=$(./plus 1 2 あ 3 4 5)
-#[ "$?" = 1 ] || ng ${LINENO}
-[ "${out}" = ERROR:数字以外が足されています 
-  "${out}" = 15 ] || ng ${LINENO}
+[ "$?" = 1 ] || ng ${LINENO}
+[ "${out}" = ERROR:数字以外が足されています 15 ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 exit $res
