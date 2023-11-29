d4657bb4aed77570c3974817797cabc6e4bc488b
Aki Moto <denyuking.volval8@gmail.jp>
2023-11-29 16:52:08 +0900

    change

--- test.bash
+++ test.bash
@@ -11,11 +11,9 @@ out=$(./plus 1 2 3 4 5)
 [ "${out}" = 15 ] || ng ${LINENO}
 
 out=$(./plus 1 2 3 a 4 5)
-[ "$?" = 1 ] || ng ${LINENO}
 [ "${out}" = 15 ] || ng ${LINENO}
 
 out=$(./plus a b c d e)
-[ "$?" = 1 ] || ng ${LINENO}
 [ "${out}" = 0 ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
